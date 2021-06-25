ad4f818171c5b4c9ef4f3b63fa98c85ac6168be1
csikos.bence <csikos.bence@gammadigital.hu>
2021-06-25 13:24:16 +0200

    Update testcases with mothod calling and expect result

--- tests.js
+++ tests.js
@@ -1,32 +1,40 @@
+const calculateDueDate = require('./calculate');
 
 describe('no or invalid input', () => {
   it('should give error when no submit date input', () => {
-
+    const result = calculateDueDate({ turnaroundTime: 16 });
+    expect(result).toThrowError('inputs are not given');
   });
   
   it('should give error when no turnaround time input', () => {
-
+    const result = calculateDueDate({ submitDate: '2021-05-18T13:50' });
+    expect(result).toThrowError('inputs are not given');
   });
   
   it('should give error when no input at all', () => {
-
+    const result = calculateDueDate();
+    expect(result).toThrowError('inputs are not given');
   });
 
   it('should give error when input submit date is not date', () => {
-
+    const result = calculateDueDate({ submitDate: undefined, turnaroundTime: 16 });
+    expect(result).toThrowError('inputs are not valid');
   });
   
   it('should give error when input turnaround time is not number', () => {
-
+    const result = calculateDueDate({ submitDate: '2021-05-18T13:50', turnaroundTime: undefined });
+    expect(result).toThrowError('inputs are not valid');
   });
   
   it('should give error when input submit date is not in working hours', () => {
-
+    const result = calculateDueDate({ submitDate: '2021-05-16T13:50', turnaroundTime: 16 });
+    expect(result).toThrowError('not working hours');
   });
 });
 
 it('should give valid output when input is valid too', () => {
-
+  const result = calculateDueDate({ submitDate: '2021-05-18T13:50', turnaroundTime: 16 });
+  expect(result).toEqual('2021-05-20T13:50');
 });
 
 // date format?
