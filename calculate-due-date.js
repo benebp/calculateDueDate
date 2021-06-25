2294a67c5137982c8ac1eba73616159cd86d1a7f
csikos.bence <csikos.bence@gammadigital.hu>
2021-06-25 14:47:31 +0200

    Update calculateDueDate

--- calculate-due-date.js
+++ calculate-due-date.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const calculateDueDate = ({ submitDate, turnaroundTime }) => {
+  // throw error when no input
+  if (!submitDate || !turnaroundTime) {
+   throw new Error('One or both inputs are not given.');
+  }
   
+  // throw error when no input
+  if (typeof turnaroundTime !== Date || typeof turnaroundTime !== Number) {
+    throw new Error('One or both given inputs are not valid.');
+  }
+
 }
 
-module.exports({ calculateDueDate });
+module.exports = calculateDueDate;
