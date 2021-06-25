21b551729509256b5729cf400dab9cadb76602ed
csikos.bence <csikos.bence@gammadigital.hu>
2021-06-25 13:02:03 +0200

    Add testcases

---
+++ tests.js
@@ -0,0 +1,33 @@
+
+describe('no or invalid input', () => {
+  it('should give error when no submit date input', () => {
+
+  });
+  
+  it('should give error when no turnaround time input', () => {
+
+  });
+  
+  it('should give error when no input at all', () => {
+
+  });
+
+  it('should give error when input submit date is not date', () => {
+
+  });
+  
+  it('should give error when input turnaround time is not number', () => {
+
+  });
+  
+  it('should give error when input submit date is not in working hours', () => {
+
+  });
+});
+
+it('should give valid output when input is valid too', () => {
+
+});
+
+// date format?
+// new Date('2021-05-18T13:50')
